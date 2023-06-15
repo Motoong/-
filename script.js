97e10a5920898bb14a0017df792549c82e9e801f
unknown <sonada903@gmail.com>
2023-06-15 19:29:44 +0900

    효과음추가 & 게임오버수정

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var gridItems = document.querySelectorAll(".grid-item");
+var container = document.getElementById("container")
 var startButton = document.getElementById("startButton");
 var scoreDisplay = document.getElementById("scoreDisplay");
 
@@ -7,12 +8,17 @@ var userSequence = [];
 var score = 0;
 var isGameActive = false;
 
+var effectSound = new Audio("sound/Blop Sound.mp3");
+var CorrectSound = new Audio("sound/Correct 2.mp3");
+var WorngSound = new Audio("sound/error 10.mp3");
+
 // 시작버튼 
 startButton.addEventListener("click", function() {
   if (!isGameActive) {
     resetGame();
     isGameActive = true;
     startButton.disabled = true;
+    container.classList.remove("game-over");
     showSequence();
   }
 });
@@ -22,7 +28,7 @@ function resetGame() {
   sequence = [];
   userSequence = [];
   score = 0;
-  scoreDisplay.textContent = "Level : " + score;
+  scoreDisplay.innerHTML = "색이 바뀌는 버튼을 기억하세요 !<br> Score: " + score;
 }
 
 //게임 진행
@@ -30,22 +36,21 @@ function showSequence() {
   var randomIndex = Math.floor(Math.random() * gridItems.length);
 
   sequence.push(randomIndex);
-
   var delay = 1000;
   sequence.forEach(function(index, idx) {
     setTimeout(function() {
-      gridItems[index].style.backgroundColor = "white";
-      
+      effectSound.play();
+      gridItems[index].style.backgroundColor = "white";     
       setTimeout(function() {
         gridItems[index].style.backgroundColor = "rgb(20, 105, 217)";
-
+        
         if (idx === sequence.length - 1) {
           enableGridClick();
         }
       }, 300);
     }, delay);
 
-    delay += 1000;
+    delay += 800;
   });
 }
 
@@ -66,7 +71,9 @@ function handleGridClick() {
   var clickedIndex = Array.from(gridItems).indexOf(this);
 
   userSequence.push(clickedIndex);
+  //effectSound.play();
   this.style.backgroundColor = "white";
+  
   
   setTimeout(function() {
     gridItems[clickedIndex].style.backgroundColor = "rgb(20, 105, 217)";
@@ -75,30 +82,35 @@ function handleGridClick() {
   var isCorrect = checkUserSequence();
   if (!isCorrect) {
     endGame();
-  // 맞췄다면
-  } else if (userSequence.length === sequence.length) { 
+  // 레벨 통과
+  } else if (userSequence.length === sequence.length) {
+    CorrectSound.play(); 
     score++;
-    scoreDisplay.textContent = "Level : " + score;
+    scoreDisplay.innerHTML = "색이 바뀌는 버튼을 기억하세요 !<br> Score: " + score;
     userSequence = [];
     disableGridClick();
     showSequence();
   }
 }
 
-// 맞췃는지 체크
+// 유저 클릭 체크
 function checkUserSequence() {
   for (var i = 0; i < userSequence.length; i++) {
-    if (userSequence[i] !== sequence[i]) {
+    if (userSequence[i] !== sequence[i]) {      
       return false;
     }
   }
+  effectSound.play();
   return true;
 }
 
 // 게임종료 시 
 function endGame() {
+  WorngSound.play();
   isGameActive = false;
   startButton.disabled = false;
   scoreDisplay.innerHTML = "Game Over! <br> Score: " + score;
   disableGridClick();
+  
+  container.classList.add("game-over");
 }
